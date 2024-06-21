92677c6abbba38e96e39726a6ff55961e8814ad1
jaineDoe <jainekudzai@gmail.com>
2024-06-21 11:43:21 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,28 +10,29 @@
  */
 int main(void)
 {
-	int hundreds, tens, ones;
+    int hundreds, tens, ones;
 
-	for (hundreds = 0; hundreds <= 7; hundreds++)
-	{
-		for (tens = hundreds + 1; tens <= 8; tens++)
-		{
-			for (ones = tens + 1; ones <= 9; ones++)
-			{
-				putchar((hundreds % 10) + '0');
-				putchar((tens % 10) + '0');
-				putchar((ones % 10) + '0');
+    for (hundreds = 0; hundreds <= 7; hundreds++)
+    {
+        for (tens = hundreds + 1; tens <= 8; tens++)
+        {
+            for (ones = tens + 1; ones <= 9; ones++)
+            {
+                putchar((hundreds % 10) + '0');
+                putchar((tens % 10) + '0');
+                putchar((ones % 10) + '0');
 
-				if (hundreds != 7 || tens != 8 || ones != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
+                if (!(hundreds == 7 && tens == 8 && ones == 9))
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
+
 
